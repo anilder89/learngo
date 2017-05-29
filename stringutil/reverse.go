5da8e9809c59b9f04b59001e9d1c7c7ddf50222f
Murat Oener <muratoener@Murats-MacBook-Pro.local>
2017-05-29 14:21:05 +0200

    pkg test

---
+++ stringutil/reverse.go
@@ -0,0 +1,12 @@
+// Utility functions for working with strings
+package stringutil
+
+// Reserve returns reserved left to right
+
+func Reverse(s string) string {
+	r:= []rune(s)
+	for i,j:=0, len(r)-1; i<len(r)/2; i,j=i+1,j-1 {
+		r[i],r[j]=r[j],r[i]
+	}
+	return string(r)
+}
